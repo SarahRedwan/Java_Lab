0d4e3188ddefa14f4eea1b9e8d8d5b8b9226c053
Sara Redwan Shure <susured22@gmail.com>
2026-05-20 22:25:20 +0300

    Lab 3

--- src/chatapp/ChatServer.java
+++ src/chatapp/ChatServer.java
@@ -6,23 +6,18 @@ import java.util.*;
 
 public class ChatServer {
 
-    // Store all connected clients
-    static List<PrintWriter> clients = new ArrayList<>();
+    static List<PrintWriter> clients = Collections.synchronizedList(new ArrayList<>());
 
     public static void main(String[] args) throws Exception {
 
         ServerSocket server = new ServerSocket(1234);
-
         System.out.println("Server started...");
 
         while (true) {
-
             Socket socket = server.accept();
-
             System.out.println("Client connected");
 
             ClientHandler clientThread = new ClientHandler(socket);
-
             clientThread.start();
         }
     }
